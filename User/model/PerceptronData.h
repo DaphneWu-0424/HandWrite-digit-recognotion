d93683adc0826d3df7d253ed781b5901cb032477
yueyingwu <3265918933@qq.com>
2026-06-25 23:36:32 +0800

    pipeline complete

--- User/model/PerceptronData.h
+++ User/model/PerceptronData.h
@@ -6,8 +6,7 @@
 #define PERCEPTRON_INPUT_SIZE 784U
 #define PERCEPTRON_CLASS_COUNT 10U
 
-/* Set to 1 by tools/train/export_perceptron.py after real MNIST training. */
-#define PERCEPTRON_MODEL_GENERATED 0
+#define PERCEPTRON_MODEL_GENERATED 1
 
 extern const float g_perceptron_weights[PERCEPTRON_CLASS_COUNT][PERCEPTRON_INPUT_SIZE];
 extern const float g_perceptron_bias[PERCEPTRON_CLASS_COUNT];
